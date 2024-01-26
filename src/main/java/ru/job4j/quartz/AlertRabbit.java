e91156e2587eb7bfeafd62cf5d145eec58abcfee
KatUser <ekaterina.shirokikh@gmail.com>
2024-01-26 09:51:31 +0700

    1.1. Job c параметрами [#260360 #476052]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,4 +1,4 @@
-package quartz;
+package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
@@ -17,13 +17,14 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
-    public static Connection connection;
-
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", Rabbit.getRabbitConnection());
             JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
                     .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(5)
@@ -41,8 +42,25 @@ public class AlertRabbit {
     }
 
     public static class Rabbit implements Job {
-
         public Rabbit() {
+        }
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            Connection connection =
+                    (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            Long jobExecutionTime = System.currentTimeMillis();
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(String.format(
+                        "INSERT INTO rabbit VALUES ('%d')",
+                        jobExecutionTime));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static Connection getRabbitConnection() {
+            Connection connection;
             try (InputStream input = Rabbit.class
                     .getClassLoader().getResourceAsStream("rabbit.properties")) {
                 Properties config = new Properties();
@@ -52,22 +70,12 @@ public class AlertRabbit {
                 String username = config.getProperty("login");
                 String password = config.getProperty("password");
                 Class.forName(drivers);
-                connection = DriverManager.getConnection(connectionURL, username, password);
+                connection =
+                        DriverManager.getConnection(connectionURL, username, password);
             } catch (IOException | ClassNotFoundException | SQLException e) {
                 throw new RuntimeException(e);
             }
-        }
-
-        @Override
-        public void execute(JobExecutionContext context) {
-            Long jobExecutionTime = System.currentTimeMillis();
-            try (Statement statement = connection.createStatement()) {
-                statement.execute(String.format(
-                        "INSERT INTO rabbit VALUES ('%d')",
-                        jobExecutionTime));
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            return connection;
         }
     }
 }
